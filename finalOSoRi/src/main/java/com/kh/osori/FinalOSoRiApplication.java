4b72684b5739935bf3a961a829a4b177e7281783
jsj <a01099350974@gmail.com>
2026-01-23 17:26:53 +0900

    feat : DB 연동 및 로그인/가입 처리 로직 구현 완료

---
+++ finalOSoRi/src/main/java/com/kh/osori/FinalOSoRiApplication.java
@@ -0,0 +1,21 @@
+package com.kh.osori;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class FinalOSoRiApplication {
+
+	public static void main(String[] args) {
+		
+		// 해시 한 번 뽑고 콘솔 복사하기
+	    System.out.println("bcrypt(1234) = " + new BCryptPasswordEncoder().encode("1234"));
+		
+		SpringApplication.run(FinalOSoRiApplication.class, args);
+		
+		
+	}
+
+}
