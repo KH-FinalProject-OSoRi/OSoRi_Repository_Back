d15d3628a2dd6edd083eea0f02b4078c57e889c4
minchaeee514 <zena514@naver.com>
2026-01-28 12:32:14 +0900

    그룹/개인 가게부 상세페이지 최종

--- finalOSoRi/src/main/java/com/kh/osori/groupBudget/model/vo/GroupBudget.java
+++ finalOSoRi/src/main/java/com/kh/osori/groupBudget/model/vo/GroupBudget.java
@@ -1,5 +1,8 @@
 package com.kh.osori.groupBudget.model.vo;
 import java.sql.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +16,7 @@ public class GroupBudget {
 	private int groupbId;
 	private int userId;
 	private String title;
+	@JsonProperty("bAmount")
 	private int bAmount;
 	private Date startDate;
 	private Date endDate;
