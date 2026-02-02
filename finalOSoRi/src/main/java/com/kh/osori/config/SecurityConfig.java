58c3158d904904dc082d68ac0d30674eea5c7e51
minchaeee514 <zena514@naver.com>
2026-02-02 15:03:44 +0900

    사진수정 완료

--- finalOSoRi/src/main/java/com/kh/osori/config/SecurityConfig.java
+++ finalOSoRi/src/main/java/com/kh/osori/config/SecurityConfig.java
@@ -1,13 +1,14 @@
 package com.kh.osori.config;
 
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //해당 클래스가 설정파일임을 알려주는 어노테이션 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
 	
 	//등록하고자 하는 bean이있다면 @Bean 어노테이션 작성
 	
@@ -18,6 +19,16 @@ public class SecurityConfig {
 		return new BCryptPasswordEncoder();
 	}
 	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    String projectPath = System.getProperty("user.dir");
+
+	    String location = "file:///" + projectPath.replace("\\", "/") + "/upload/profiles/";
+
+	    registry.addResourceHandler("/upload/profiles/**")
+	            .addResourceLocations(location);
+	}
 }
+	
 	
 	
