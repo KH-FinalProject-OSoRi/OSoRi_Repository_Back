4b71bea6773ebe408f9f407d513258148b2a0181
jsj <a01099350974@gmail.com>
2026-02-03 15:40:50 +0900

    feat : 소셜 로그인 구현

--- finalOSoRi/src/main/java/com/kh/osori/user/model/service/UserServiceImpl.java
+++ finalOSoRi/src/main/java/com/kh/osori/user/model/service/UserServiceImpl.java
@@ -1,15 +1,25 @@
 package com.kh.osori.user.model.service;
 
+import java.util.HashMap;
+
 import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import com.kh.osori.user.model.dao.UserDao;
+import com.kh.osori.user.model.dto.UserRegisterRequest;
 import com.kh.osori.user.model.vo.User;
+import com.kh.osori.util.JwtUtil;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -20,12 +30,34 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 	
+	@Autowired
+	private JwtUtil jwtUtil; // 토큰 발급용 
+	
+	@Transactional
 	@Override // 회원 가입 
-	public int insertUser(User user) {
+	public int insertUser(UserRegisterRequest request) {
 		
-		int result = dao.insertUser(sqlSession, user);
+		User user = request.getUser(); 
 		
-		return result; 
+		String loginType = request.getLoginType();
+		
+		String providerUserId = request.getProviderUserId();
+		
+		int result1 = dao.insertUser(sqlSession, user);
+		
+		int currentUserId = dao.selectUserId(sqlSession);
+		
+		HashMap<String, Object> accountMap = new HashMap<>();
+		
+		accountMap.put("loginType", loginType);
+		accountMap.put("providerUserId", providerUserId);
+		accountMap.put("currentUserId", currentUserId);
+		
+		int result2 = dao.insertAuthAccount(sqlSession, accountMap); 
+		
+		int sum = result1 + result2; 
+		
+		return sum; 
 		
 	}
 	
@@ -36,8 +68,6 @@ public class UserServiceImpl implements UserService {
 		int result = 0; // 트랜잭션 처리를 한 후, 결과값 받는 변수 
 		
 		User loginUser = dao.selectUser(sqlSession, user); // 사용자가 입력한 아이디 및 비밀번호를 데이터 베이스에서 있는지 조회.
-		
-		
 		
 		//위에 로그인 멤버는 데이터베이스에서 조회만 해온거지 휴면 처리 이런건 전혀 안되어있다. 
 		
@@ -64,6 +94,74 @@ public class UserServiceImpl implements UserService {
 		
 		return null; // 이 경우는 loginMember가 null이든 아니든 마지막 로그인 날짜 시점을 처리하지 못하면 실패로 처리. 
 		 
+	}
+	
+	/* UserServiceImpl.java */
+
+	@Override
+	public Map<String, Object> processKakaoLogin(String code) {
+	    
+		// 카카오 토큰 받기 
+	    RestTemplate rt = new RestTemplate();
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+	    
+	    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+	    params.add("grant_type", "authorization_code");
+	    params.add("client_id", "fbeeefb1ab0d16e849dfdfdd01f9222b"); //
+	    params.add("redirect_uri", "http://localhost:5173/auth/kakao/callback");
+	    params.add("code", code);
+
+	    // 카카오 보안 설정에서 Client Secret을 생성했다면 반드시 아래 코드를 추가해야 401 에러가 안 납니다.
+	   
+	    HttpEntity<MultiValueMap<String, String>> tokenRequest = new HttpEntity<>(params, headers);
+	    ResponseEntity<Map> tokenResponse = rt.exchange("https://kauth.kakao.com/oauth/token", HttpMethod.POST, tokenRequest, Map.class);
+	    String accessToken = (String) tokenResponse.getBody().get("access_token");
+
+	    // 2. 사용자 정보 가져오기 (닉네임 + 이메일)
+	    headers = new HttpHeaders();
+	    headers.add("Authorization", "Bearer " + accessToken);
+	    HttpEntity<MultiValueMap<String, String>> profileRequest = new HttpEntity<>(headers);
+	    ResponseEntity<Map> profileResponse = rt.exchange("https://kapi.kakao.com/v2/user/me", HttpMethod.POST, profileRequest, Map.class);
+	    
+	    Map<String, Object> body = profileResponse.getBody();
+	    Map<String, Object> kakaoAccount = (Map<String, Object>) body.get("kakao_account");
+	    Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+	    
+	    String email = (String) kakaoAccount.get("email"); //에서 권한을 얻어야 null이 안 나옵니다.
+	    String nickName = (String) profile.get("nickname"); //
+	    
+	    String providerUserId = String.valueOf(body.get("id")); // 고유 토큰 아이디 
+	    String loginType = "KAKAO"; // 로그인 타입 
+
+	    // 3. DB 가입 확인 및 처리 (이메일 기준)
+	    User user = dao.findLoginIdByEmail(sqlSession, email); //
+	    
+	    Map<String, Object> result = new HashMap<>();
+	    
+	    if (user == null) {
+	        
+	    	
+	    		result.put("isNewMember", true); // 추가
+	    		result.put("email",email);
+	    		result.put("nickName", nickName); 
+	    		result.put("providerUserId", providerUserId); // 토큰 고유 아이디 
+	    		result.put("loginType","KAKAO"); 
+	    		
+	    		return result; 
+	    	
+	    	
+	    }
+	    
+	    // 4. 전용 JWT 발행
+	    String token = jwtUtil.generateToken(user.getLoginId());
+	    user.setPassword(null);
+	    
+	    
+	    result.put("token", token);
+	    result.put("user", user);
+	    return result;
 	}
 	
 	
