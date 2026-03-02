b13e0b454b35a95076407e66575b666e8cb69a9c
annnerss <annaseo6375@gmail.com>
2026-03-02 23:52:42 +0900

    Fix. 경로 수정

--- finalOSoRi/src/main/java/com/kh/osori/badge/controller/BadgeController.java
+++ finalOSoRi/src/main/java/com/kh/osori/badge/controller/BadgeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kh.osori.badge.model.vo.Badge;
 import com.kh.osori.badge.service.BadgeService;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/badges")
 public class BadgeController {
